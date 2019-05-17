03360d4642ea6d2eee83a00d9f65c672ec00fbb5
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-05-17 22:57:32 +0200

    Progreso en roomManager

--- practica_2018-19/src/main/java/spacewar/Room.java
+++ practica_2018-19/src/main/java/spacewar/Room.java
@@ -8,46 +8,49 @@ public class Room {
 	ConcurrentHashMap<Integer, Player> playerMap;
 
 	public enum State {
-		Waiting, Playing
+		Waiting, Full, Playing
 	}
 
 	public State state;
 
 	private AtomicInteger peopleInside;
 	private final int capacity;
+	
+	private RoomManager roomManager;
 
-	public Room(int capacity) {
+	public Room(int capacity, RoomManager roomManager) {
 		this.peopleInside = new AtomicInteger(0);
 		this.capacity = capacity;
 		this.state = State.Waiting;
-		this.playerMap = = new ConcurrentHashMap<Integer, Player>();
+		this.playerMap = new ConcurrentHashMap<Integer, Player>();
+		this.roomManager = roomManager;
 	}
 
 	public boolean addPlayer(Player player) {
-			if (peopleInside.get() < capacity) {
-				playerMap.putIfAbsent(player.getPlayerId(), player);
-				peopleInside.incrementAndGet();
-				return true;
-			} else
-				return false;
+		if (peopleInside.get() < capacity) {
+			playerMap.putIfAbsent(player.getPlayerId(), player);
+			peopleInside.incrementAndGet();
+			return true;
+		} else
+			return false;
 	}
-	
-	public void RemovePlayer(Player player)
-	{
+
+	public void RemovePlayer(Player player) {
 		peopleInside.decrementAndGet();
 		playerMap.remove(player.getPlayerId());
-			
+
 	}
-	
-	//No sé si se usará o no este método. En caso de que no, se borrará.
-	public int getPeopleInside()
-	{
+
+	// No sé si se usará o no este método. En caso de que no, se borrará.
+	public int getPeopleInside() {
 		return peopleInside.get();
 	}
-	
-	public void PreMatchLobbyThread()
-	{
-		
+
+	public void PreMatchLobbyThread() {
+		do
+		{
+			
+		}while(this.state!=State.Playing);
 	}
 
 }
--- practica_2018-19/src/main/java/spacewar/RoomManager.java
+++ practica_2018-19/src/main/java/spacewar/RoomManager.java
@@ -1,24 +1,43 @@
 package spacewar;
 
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class RoomManager {
-	private final int waitingQueueCapacity;
 	private Executor roomExecutor;
-	private BlockingQueue waitingRooms;
-	
-	public RoomManager(int waitingQueueCapacity)
-	{
-		this.waitingQueueCapacity = waitingQueueCapacity;
-		this.waitingRooms = new ArrayBlockingQueue(this.waitingQueueCapacity);
+	private final int roomCapacity = 6;
+	private BlockingQueue<Room> waitingRooms;
+	private BlockingQueue<Room> fullRooms;
+
+	public RoomManager() {
 		this.roomExecutor = Executors.newCachedThreadPool();
+		waitingRooms = new LinkedBlockingQueue<Room>();
+		fullRooms = new LinkedBlockingQueue<Room>();
 	}
-	
-	public void StartRoom(Room room) {
-		roomExecutor.execute(()->room.PreMatchLobbyThread());
+
+	public void ConnectNewPlayer(Player player) {
+		synchronized (waitingRooms) {
+			if (!waitingRooms.peek().addPlayer(player)) {
+				if (waitingRooms.peek().getPeopleInside() == roomCapacity) {
+					try {
+						fullRooms.add(waitingRooms.take());
+						Room room = new Room(roomCapacity, this);
+						roomExecutor.execute(() -> room.PreMatchLobbyThread());
+					} catch (InterruptedException e) {
+						System.out.println("Error al añadir al jugador a una sala, intentando de nuevo.");
+						ConnectNewPlayer(player);
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 	}
-	
+
+	public void deleteRoom(Room room) {
+		if (!waitingRooms.remove(room))
+			fullRooms.remove(room);
+	}
+
 }
